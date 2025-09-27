1f28c1b3951c5fa78daa9a024874d8e70f883a04
Ivan Kozlovskyi <ivan.kz5565@gmail.com>
2025-09-27 22:32:58 +0300

    Vue.js lab 1

---
+++ lab1/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  
+  <h1>Kozlovksyi Ivan Viktorovych</h1>
+  <h2>Specialty: Software Engineering</h2>
+  <h2>Group: 8.1214</h2>
+  <h2>2nd year of master's degree.</h2>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
