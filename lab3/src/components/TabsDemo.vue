967e49dcd9b3584db974ba174bd72110b09262ca
Ivan Kozlovskyi <ivan.kz5565@gmail.com>
2025-11-03 13:24:27 +0200

    update lab3

--- lab3/src/components/TabsDemo.vue
+++ lab3/src/components/TabsDemo.vue
@@ -2,22 +2,30 @@
 import { ref } from 'vue'
 import Tabs from './Tabs.vue'
 import Tab from './Tab.vue'
-const current = ref('profile')
-const another = ref('profile')
+const current1 = ref('profile')
+const current2 = ref('profile')
+const current3 = ref('profile')
 </script>
 
 <template>
-  <div class=" border border-black p-4 rounded-xl">
-    <Tabs v-model="current" variant="underline">
+  <div class="border border-black p-4 rounded-xl">
+    <Tabs v-model="current1" variant="underline">
     <Tab title="Профіль" slug="profile">Вміст профілю...</Tab>
     <Tab title="Налаштування" slug="settings">Форма налаштувань...</Tab>
     <Tab title="Контакти" slug="contacts">Контактна інформація...</Tab>
   </Tabs>
   </div>
-  <div class=" border border-black p-4 rounded-xl">
-    <Tabs v-model="another" variant="pills">
+  <div class="border border-black p-4 rounded-xl">
+    <Tabs v-model="current2" variant="pills">
       <Tab title="Info" slug="info">Інфо...</Tab>
       <Tab title="Data" slug="data">Дані...</Tab>
+    </Tabs>
+  </div>
+  <div class="border border-black p-4 rounded-xl">
+    <Tabs v-model="current3" variant="boxed">
+      <Tab title="Info" slug="info">Інфо...</Tab>
+      <Tab title="Data" slug="data">Дані...</Tab>
+      <Tab title="Other" slug="other">Інше...</Tab>
     </Tabs>
   </div>
 
